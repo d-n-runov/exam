eaaf249a1d0c1146c2a3e29a0ac5177785d1bfd0
runov-daniil <runov-daniil@yandex.ru>
2018-04-16 11:25:15 +0300

    Создна БД, настройки связи с ней

--- SrvExam/src/srvexam/dataBase.java
+++ SrvExam/src/srvexam/dataBase.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package srvexam;
 
-/**
- *
- * @author Irbis
- */
+import java.sql.*;
+
 public class dataBase {
+    public static Connection conn;
+    public static Statement statmt;
+    public static ResultSet resSet;
     
+    public static void Conn() throws  ClassNotFoundException, SQLException {
+        conn = null;
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:data.s3db");
+        System.out.println("DONE!");
+    }
 }
